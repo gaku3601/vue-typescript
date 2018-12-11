bb2821a9c52335da165757398f7cc8d751062956
gaku3601 <pro.gaku@gmail.com>
2018-12-11 14:24:48 +0900

    typescriptでvuexとcomponentを実装

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+    {{incrementCounter}}
+    <button @click="increment">add</button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
@@ -8,11 +10,19 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { counterModule } from '@/store/modules/counter';
 
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  get incrementCounter() {
+    return counterModule.incrementCounter; // インテリセンスが効く
+  }
+  increment() {
+    counterModule.SET_INCREMENT_COUNTER(counterModule.incrementCounter + 1); // 型チェックが効く
+  }
+}
 </script>
